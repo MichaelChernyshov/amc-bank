bb44788fa3412722483ece0880565c0501c278cb
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-05-09 15:15:38 +0200

    adding dist

---
+++ dist/assets/DepositView-52ca9bb3.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,a as s}from"./index-92189b44.js";const c={},n=s("h1",null,"Deposit",-1),_=[n];function a(r,i){return o(),t("div",null,_)}const d=e(c,[["render",a]]);export{d as default};
---
+++ dist/assets/LoansView-fea36e84.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as c}from"./index-92189b44.js";const n={},t=c("h1",null,"Loans",-1),a=[t];function _(r,l){return o(),s("div",null,a)}const i=e(n,[["render",_]]);export{i as default};
