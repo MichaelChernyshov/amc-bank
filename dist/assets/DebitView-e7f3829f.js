81981e1a0028cb5cfdd2c87373f2c52df96c288c
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-05-10 23:42:05 +0200

    Deleted dist

--- dist/assets/DebitView-e7f3829f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c,a as o}from"./index-92189b44.js";const s={},n=o("h1",null,"Debit card",-1),a=[n];function r(_,i){return t(),c("div",null,a)}const l=e(s,[["render",r]]);export{l as default};
--- dist/assets/SimCardView-bfc0e740.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o,a as t}from"./index-92189b44.js";const s={},a=t("h1",null,"Sim-Card",-1),n=[a];function r(_,d){return c(),o("div",null,n)}const l=e(s,[["render",r]]);export{l as default};
