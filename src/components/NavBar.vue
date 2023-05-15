dd548882110db3a063074a2ac02ec457b4dcec58
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-05-15 19:31:25 +0200

    Added variables, new svg. Editted nav bar.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,31 +1,29 @@
 <template>
 <header>
-    <div class="nav-bar">
-        <div>
-            <span>
+    <div class="navigation">
+        <div class="nav-logo">
                 <a href="/">
                     <div class="logo"></div>
                 </a>
-            </span>
         </div>
 
-        <div>
-            <nav>
+        <div class="nav-bar">
+            <nav class="nav_bar__link">
                 <RouterLink to="/debit">Debit Card</RouterLink>
                 <RouterLink to="/credit">Credit Card</RouterLink>
                 <RouterLink to="/loans">Loans</RouterLink>
                 <RouterLink to="/deposit">Deposit</RouterLink>
                 <RouterLink to="/sim-card">Sim-Card</RouterLink>
             </nav>
+
+            <div class="nav_bar__enter">
+                <a href="/" class="btn-enter">
+                    <p>Enter</p>
+                </a>
+            </div>
         </div>
 
-        <div>
-            <span>
-                <a href="/">
-                    <div class=""></div>
-                </a>
-            </span>
-        </div>
+        
         
     </div>
       
@@ -35,18 +33,52 @@
 </template>
 
 <style lang="scss">
-    .logo {
-        max-width: 5rem;
-        background: no-repeat url(../assets/images/logo/logoBlack.svg);
-        width: 100px;
-        height: 30px;
-    }    
-    .nav-bar {
-        margin: 2%;
+    .navigation {
+        padding: 2%;
         display: flex;
         justify-content: space-between;
+        color: white;
     }
-    .nav-div {
+
+    .nav-logo {
+
+        .logo {
+            background: no-repeat url(../assets/images/logo/logoWhite.svg);
+            width: 80px;
+            height: 30px;
+        } 
+    }
+    .nav-bar {
         display: flex;
+        align-items: center;
+        
+        .nav_bar__link {
+            display: flex;
+
+            a {
+                margin-left: 2rem;
+                font-size: 10px;
+
+                &:first-child {
+                    margin-left: 0;
+                }
+            }
+        }
+    }
+
+    .nav_bar__enter {
+        align-items: center;
+        display: flex;
+        margin-left: 2rem;
+
+        .btn-enter {
+        border: 1px solid rgb(12, 24, 81);
+        border-radius: 28px;
+        font-family: Arial;
+        color: #ffffff;
+        font-size: 10px;
+        padding: 10px 20px 10px 20px;
+        text-decoration: none;
+        }
     }
 </style>
--- src/components/WelcomeComp.vue
+++ src/components/WelcomeComp.vue
@@ -5,7 +5,7 @@
 <div>
     <h1 class="welcome-title">Premium Bank For You</h1>
     <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pellentesque dui eget libero faucibus hendrerit. Quisque ut viverra mauris. Sed maximus tempus porta. </h2>
-    <img src="../assets/images/icons/creee.svg" alt="">
+    <!-- <img src="../assets/images/icons/creee.svg" alt=""> -->
 </div>
 </template>
 
