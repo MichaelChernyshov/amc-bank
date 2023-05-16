16c3d33d26323f911cedff3db3dbb4808ac516f6
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-05-16 17:33:02 +0200

    Added yarn with vuetify

--- src/components/DebitCard.vue
+++ src/components/DebitCard.vue
@@ -1,5 +1,17 @@
 <script>
-
+export default {
+    data() {
+        return {
+            isBtnPopCloseFocused: false
+        }
+    },
+    methods: {
+        toggleToItemEditForm() {
+        console.log(this.$refs.editButton);
+        this.isEditing = true;
+        }
+    }
+}
 </script>
 
 <template>
@@ -8,9 +20,28 @@
             <h1 class="debit__welcome">Debit cards</h1>
             <p>Using debit card is the best way to pay for everything you want and after all - receive a cashback.</p>
         </article>
+
+        <section>
+            <div class="debit__buttons">
+                <button type="button" class="button" ref="normButton" @click="toggleToItemEditForm">Normal</button>
+                <button type="button" class="button" ref="premButton" @click="toggleToItemEditForm">Premium</button>
+            </div>
+        </section>
     </main>
 
 </template>
 
 <style lang="scss">
+
+.debit__buttons {
+    display: flex;
+    justify-content: space-around;
+    background-color: grey;
+    width: 30%;
+    height: 20px;
+
+    .button:focus {
+        background-color: red;
+    }
+}
 </style>
