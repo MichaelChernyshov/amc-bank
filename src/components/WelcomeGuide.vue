e07c5f678c33fa9a3f97f049f5b3f42180024437
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-05-17 16:45:32 +0200

    Added new comp to main page

---
+++ src/components/WelcomeGuide.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+<div class="try-div">
+    <img src="../assets/images/icons/iphone-icon.svg" alt="Card Credit" class="try">
+    <img src="../assets/images/icons/CardsQQ.svg" alt="" class="try">
+</div>
+</template>
+
+<style lang="scss">
+
+.try {
+    width: 30%;
+    height: 30%;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import WelcomeComp from '../components/WelcomeComp.vue'
+import WelcomeGuide from '../components/WelcomeGuide.vue';
 </script>
 
 <template>
   <WelcomeComp></WelcomeComp>
+  <WelcomeGuide></WelcomeGuide>
 </template>
