def22787765f17968a379d16064b742e9e7b53e9
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-05-17 16:42:09 +0200

    DRY, added download buttons.

--- src/components/WelcomeComp.vue
+++ src/components/WelcomeComp.vue
@@ -5,31 +5,19 @@
 <main class="main">
     <article class="main__main-info">
         <h1 class="main__welcome">Pay everywhere you want with a cashback</h1>
-        <figure class="cube">
-        </figure>
+        <div class="main__donload-buttons">
+            <v-btn variant="outlined" rounded="xl" size="x-small" prepend-icon="mdi-apple" class="mr-3">
+                AppStore
+            </v-btn>
+            <v-btn variant="outlined" rounded="xl" size="x-small" prepend-icon="mdi-google-play">
+              Google Play
+            </v-btn>
+        </div>
     </article>
 </main>
-
-<div class="try-div">
-    <img src="../assets/images/icons/iphone-icon.svg" alt="Card Credit" class="try">
-    <img src="../assets/images/icons/CardsQQ.svg" alt="" class="try">
-</div>
-
-<v-btn prepend-icon="mdi-apple">
-  Button
-</v-btn>
-<v-btn prepend-icon="mdi-google">
-  Button
-</v-btn>
 </template>
 
 <style lang="scss">
-
-.try {
-    width: 30%;
-    height: 30%;
-}
-
 .main {
     width: 100vw;
     height: 84vh;
@@ -48,19 +36,4 @@
         }
     }
 }
-
-// .credit {
-//     width: 210px;
-//     height: 130px;
-//     transform: scaleX(1.1) scaleY(1.1) scaleZ(1) rotateX(41deg) rotateY(-7deg) rotateZ(10deg) translateX(16px) translateY(-2px) translateZ(0px) skewX(0deg) skewY(0deg);
-// }
-
-// .cube {
-// perspective: 1000px;
-// perspective-origin: 50% 50%;
-// }
-
-// .shadow {
-//     box-shadow: 0px 0px 30px 30px rgba(42, 186, 199, 0.3);
-// }
 </style>
