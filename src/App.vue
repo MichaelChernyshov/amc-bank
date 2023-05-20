ac1fcf7332f246c7caef044afd30a706ee299b8a
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-05-20 22:17:56 +0200

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,16 @@ import FooterMain from './components/FooterMain.vue'
 </script>
 
 <template>
-  <NavBar />
-  <RouterView />
-  <FooterMain />
+  <v-app>
+    <NavBar />
+    <RouterView />
+    <FooterMain />
+  </v-app>
 </template>
+
+<style lang="scss">
+.v-application {
+  background: radial-gradient(100% 100% at -22% -14%, #1D38C2FF 0%, #FF000000 85%),radial-gradient(100% 100% at 116% 118%, #1D38C2FF 0%, #FF000000 85%),linear-gradient(0deg, #1E1E20FF 0%, #111010FF 94%) !important;
+  color: white !important;
+  display: block !important;
+}</style>
--- src/components/FooterMain.vue
+++ src/components/FooterMain.vue
@@ -1,28 +1,42 @@
 <script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
 </script>
 
 <template>
     <v-footer class="d-flex flex-column">
-      <div class="bg-teal d-flex w-100 align-center px-4">
-        <strong>Get connected with us on social networks!</strong>
-  
-        <v-spacer></v-spacer>
-  
+      <div class="d-flex w-100 px-4">
+        <div class="px-4 py-2 w-100 footer-text">
+        {{ new Date().getFullYear() }} AMC - Bank
+        </div>
         <v-btn
           v-for="icon in icons"
           :key="icon"
           class="mx-4"
           :icon="icon"
           variant="plain"
-          size="small"
+          size="x-small"
         ></v-btn>
-      </div>
-  
-      <div class="px-4 py-2 bg-black text-center w-100">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
       </div>
     </v-footer>
   </template>
 
 <style lang="scss">
+.v-footer {
+    color: white !important;
+    padding: 0 !important;
+    background: none !important;
+}
+.footer-text {
+    font-size: 13px;
+    color: #BDBDBD;
+}
 </style>
