3ed5b3b55db663db3cbdb02acbef0c0a0bf6e614
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-05-21 01:34:43 +0200

    Changed info

--- src/views/CreditView.vue
+++ src/views/CreditView.vue
@@ -10,6 +10,6 @@ import GuidesMain from '../components/GuidesMain.vue';
   <CardsTitle cardType="Credit" cardDescription="Allows you to pay for purchases at the expense of the bank. Pay with a card and get cashback for any purchases"></CardsTitle>
   <!-- <CardsButtons cardName="4U Credit" cardType="Credit"></CardsButtons> -->
   <CardDescription cardName="Aurora" description="Using credit card you can buy whatever you want with special offers" imgLink="/src/assets/images/cards/CardCredit.svg"></CardDescription>
-  <GuidesMain pageTitle="Conditions" fCardTitle="Cashback" fCardDescription="Up to 15% of cashback from each payment" fImgLink="/src/assets/images/icons/cards-description/cashback.svg" sCardTitle="Deposit" sCardDescription="Up to 5% annual for saving on your card" sImgLink="/src/assets/images/icons/cards-description/deposit.svg" tCardTitle="Commission" tCardDescription="No taxes and fees for using your cards" tImgLink="/src/assets/images/icons/cards-description/commission.svg"></GuidesMain>
+  <GuidesMain pageTitle="Conditions" fCardTitle="Cashback" fCardDescription="Up to 30% of cashback from each payment" fImgLink="/src/assets/images/icons/cards-description/cashback.svg" sCardTitle="Deposit" sCardDescription="Up to 5% annual for saving on your card" sImgLink="/src/assets/images/icons/cards-description/deposit.svg" tCardTitle="Commission" tCardDescription="No taxes and fees for using your cards" tImgLink="/src/assets/images/icons/cards-description/commission.svg"></GuidesMain>
   <OrderFormCredit></OrderFormCredit>
 </template>
