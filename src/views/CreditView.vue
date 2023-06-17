2500f4516adb98833e9ebaf1d60f2b4cea1f4c3f
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-06-17 11:09:03 +0200

    Changed path

--- src/views/CreditView.vue
+++ src/views/CreditView.vue
@@ -12,17 +12,17 @@ import GuidesMain from '../components/GuidesMain.vue';
   <CardDescription 
   cardName="Aurora" 
   description="Using credit card you can buy whatever you want with special offers" 
-  imgLink="../../../public/images/cards/CardCredit.svg"></CardDescription>
+  imgLink="../../public/images/cards/CardCredit.svg"></CardDescription>
   <GuidesMain 
   pageTitle="Conditions" 
   fCardTitle="Cashback" 
   fCardDescription="Up to 30% of cashback from each payment" 
-  fImgLink="../../../public/images/icons/cards-description/cashback.svg" 
+  fImgLink="../../public/images/icons/cards-description/cashback.svg" 
   sCardTitle="Deposit" 
   sCardDescription="Up to 5% annual for saving on your card" 
-  sImgLink="../../../public/images/icons/cards-description/deposit.svg" 
+  sImgLink="../../public/images/icons/cards-description/deposit.svg" 
   tCardTitle="Commission" 
   tCardDescription="No taxes and fees for using your cards" 
-  tImgLink="../../../public/images/icons/cards-description/commission.svg"></GuidesMain>
+  tImgLink="../../public/images/icons/cards-description/commission.svg"></GuidesMain>
   <OrderFormCredit></OrderFormCredit>
 </template>
